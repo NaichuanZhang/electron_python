5850f039f0155ae35fb4ec42233629b28f2d5eff
Naichuan Zhang <nzhang@bu.edu>
2017-02-28 04:50:34 -0500

    update hello.py

--- hello.py
+++ hello.py
@@ -15,16 +15,19 @@ def hi():
 	send_message()
 	return "hello from naiqian"
 
-
-
 def send_message():
 	account_sid = "AC8c82b8a8bad4219098310d27664c0fa8" # Your Account SID from www.twilio.com/console
 	auth_token  = "6d27b2410dfdf5748a66ebaef8a69baf"  # Your Auth Token from www.twilio.com/console
 	client = TwilioRestClient(account_sid, auth_token)
-	message = client.messages.create(body="Hello from Python",
+	message1 = client.messages.create(body="Hello from Python",
                                      to="+12022504356",    # Replace with your phone number
                                      from_="+12196413721") # Replace with your Twilio number
-	print(message.sid)
+	client = TwilioRestClient(account_sid, auth_token)
+	message2 = client.messages.create(body="Hello from Python",
+                                     to="+12196285761",    # Replace with your phone number
+                                     from_="+12196413721") # Replace with your Twilio number
+	print(message1.sid)
+	print(message2.sid)
 
 
 if __name__ == "__main__":
