600257e03afe8054a30047a9e38f0b159d3075be
Naichuan Zhang <nzhang@bu.edu>
2017-02-27 21:13:19 -0500

    add templates

--- hello.py
+++ hello.py
@@ -1,15 +1,33 @@
 
 from __future__ import print_function
 import time
-from flask import Flask
+from flask import Flask, Response, request, render_template, redirect, url_for
+from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! This is powered by Python backend."
+    return render_template("home_page.html")
+
+@app.route("/naiqian")
+def hi():
+	send_message()
+	return "hello from naiqian"
+
+
+
+def send_message():
+	account_sid = "AC8c82b8a8bad4219098310d27664c0fa8" # Your Account SID from www.twilio.com/console
+	auth_token  = "6d27b2410dfdf5748a66ebaef8a69baf"  # Your Auth Token from www.twilio.com/console
+	client = TwilioRestClient(account_sid, auth_token)
+	message = client.messages.create(body="Hello from Python",
+                                     to="+12022504356",    # Replace with your phone number
+                                     from_="+12196413721") # Replace with your Twilio number
+	print(message.sid)
+
 
 if __name__ == "__main__":
     print('oh hello')
     #time.sleep(5)
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=5000)
